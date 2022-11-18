0f6ec1fe98d5bf51ad593e25859cde2008f67621
nancy4hany <nancyhany8@gmail.com>
2022-11-18 23:33:22 +0200

    setting up the model files

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -2,11 +2,24 @@
 
 namespace App\Http\Controllers;
 use App\Models\User;
+use Illuminate\Http\Request;
 
 class TestController extends Controller
 {
     public function index(){
-        return User::all();
+        // return User::find($id);
+        // return User::where('id','>=','1')->get();
+        // $User = new User();
+        // $User->username="Nancy";
+        // $User->email="nancy@gmail.com";
+        // $User->password="nfvr";
+        // $User->save();
+        // $User = User::find(1);
+        // $User->username="Ahmed Syam";
+        // $User->save();
+$User = User::find(4);
+$User->delete();
+
     }
 
     public function home(){
